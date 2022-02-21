9964341cea78addb0dcd40a8704438ab12e5e699
ekater1na <ekaterina_l@tut.by>
2022-02-21 22:46:51 +0300

    feat: refactor core and shared modules

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,13 +1,26 @@
-import { NgModule, Optional, SkipSelf } from '@angular/core';
+import {ErrorHandler, NgModule, Optional, SkipSelf} from '@angular/core';
 
 import {HttpClientModule} from '@angular/common/http';
-import {EnsureModuleLoadedOnceGuard} from './ensure-module-loaded-once.guard';
+import {EnsureModuleLoadedOnceGuard} from './guards/ensure-module-loaded-once.guard';
+import {LoggerService} from './services/logger.service';
+import {DataService} from './services/data.service';
+import {BookTrackerErrorHandlerService} from './services/book-tracker-error-handler.service';
 
 @NgModule({
   imports: [HttpClientModule],
   exports: [HttpClientModule],
   declarations: [],
   providers: [
+    // PlainLoggerService,
+    // {provide: LoggerService, useExisting: PlainLoggerService},
+    // { provide: LoggerService, useValue: {
+    //   log: (message: string) => console.log(`MESSAGE: ${message}`),
+    //   error: (message: string) => console.error(`PROBLEM: ${message}`),
+    //   }},
+    // { provide: DataService, useFactory: dataServiceFactory, deps: [LoggerService] },
+    LoggerService,
+    DataService,
+    {provide: ErrorHandler, useClass: BookTrackerErrorHandlerService}
   ] // these should be singleton
 })
 export class CoreModule extends EnsureModuleLoadedOnceGuard {    // Ensure that CoreModule is only loaded into AppModule
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,6 @@
-import {ErrorHandler, NgModule} from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { CapitalizePipe } from './pipes/capitalize.pipe';
-import {LoggerService} from '../core/services/logger.service';
-import {DataService} from '../core/services/data.service';
-import {BookTrackerErrorHandlerService} from '../core/services/book-tracker-error-handler.service';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {CapitalizePipe} from './pipes/capitalize.pipe';
 
 @NgModule({
   exports: [CommonModule, CapitalizePipe],
@@ -11,17 +8,7 @@ import {BookTrackerErrorHandlerService} from '../core/services/book-tracker-erro
   declarations: [
     CapitalizePipe
   ],
-  providers: [
-    // PlainLoggerService,
-    // {provide: LoggerService, useExisting: PlainLoggerService},
-    // { provide: LoggerService, useValue: {
-    //   log: (message: string) => console.log(`MESSAGE: ${message}`),
-    //   error: (message: string) => console.error(`PROBLEM: ${message}`),
-    //   }},
-    // { provide: DataService, useFactory: dataServiceFactory, deps: [LoggerService] },
-    LoggerService,
-    DataService,
-    {provide: ErrorHandler, useClass: BookTrackerErrorHandlerService}
-  ]
+  providers: []
 })
-export class SharedModule { }
+export class SharedModule {
+}
